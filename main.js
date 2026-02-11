f8f9b907dffd1d9e1ff27bf8f36bea16c5ca995a
KinggPeculiar <KingsleyPeculiar04@gmail.com>
2026-02-11 13:22:40 +0100

    chore: structure remodel, file name and paths change - clear internal styling in index.html, modify script tag and set type to module - move script.js to root dir and rename to main.js, modify content and set import for component js files. Call functions to run code.

---
+++ main.js
@@ -0,0 +1,7 @@
+import { inputBox } from "./js/inputBox.js";
+import { taskContainer } from "./js/taskContainer.js";
+import { scrollBar } from "./js/scrollBar.js";
+
+inputBox();
+taskContainer();
+scrollBar();
